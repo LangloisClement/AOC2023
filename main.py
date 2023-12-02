de017175a829121d56b23e3492978454d08ee27b
Langlois Clément <langlois.clement@gmail.com>
2023-12-02 16:58:04 +0100

    day 2 part1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from pkg_resources import resource_stream
 from functools import reduce
 from re import sub
+import re
 
 print("hello world")
 
@@ -32,9 +33,42 @@ def day1_part2():
                 for k, v in transTable.items():
                     if line.startswith(k, i):
                         numbers.append(v)
+                        break
             lineNumber = "".join([numbers[0], numbers[-1]])
             listNumber.append(int(lineNumber))
     return reduce(lambda a, b: a+b, listNumber)
 
+def day2_part1():
+    print("day 2")
+    possibleGame=[]
+    with resource_stream('input', 'D2.txt') as textInput:
+        for line in textInput.readlines():
+            line = line.decode().removesuffix("\n")
+            lineSplit=line.split(":")
+            gameID=int(lineSplit[0].strip("Game "))
+            cubeSets=lineSplit[-1].split(";")
+            flagValide=True
+            for cubeSet in cubeSets:
+                if not flagValide:
+                    break
+                cubes=cubeSet.strip().split(", ")
+                for cube in cubes:
+                    if not flagValide:
+                        break
+                    nb=int(*(re.findall("(\d+)",cube)))
+                    match cube:
+                        case str(a) if "red" in a:
+                            if nb>12:
+                                flagValide=False
+                        case str(a) if "green" in a:
+                            if nb>13:
+                                flagValide=False
+                        case str(a) if "blue" in a:
+                            if nb>14:
+                                flagValide=False
+            if flagValide: possibleGame.append(gameID)
+    return reduce(lambda a,b: a+b, possibleGame)
 
-print(day1_part2())
+
+
+print(day2_part1())
