e717bd84777da1fed16903ea1851953b7bdaeaf4
Langlois Clément <langlois.clement@gmail.com>
2023-12-02 17:05:37 +0100

    day 2 part 2

--- main.py
+++ main.py
@@ -69,6 +69,31 @@ def day2_part1():
             if flagValide: possibleGame.append(gameID)
     return reduce(lambda a,b: a+b, possibleGame)
 
+def day2_part2():
+    print("day 2")
+    gamePower=[]
+    with resource_stream('input', 'D2.txt') as textInput:
+        for line in textInput.readlines():
+            line = line.decode().removesuffix("\n")
+            lineSplit=line.split(":")
+            cubeSets=lineSplit[-1].split(";")
+            gameSet={"red":0,"green":0,"blue":0}
+            for cubeSet in cubeSets:
+                cubes=cubeSet.strip().split(", ")
+                for cube in cubes:
+                    nb=int(*(re.findall("(\d+)",cube)))
+                    match cube:
+                        case str(a) if "red" in a:
+                            if nb>gameSet["red"]:
+                                gameSet["red"]=nb
+                        case str(a) if "green" in a:
+                            if nb>gameSet["green"]:
+                                gameSet["green"]=nb
+                        case str(a) if "blue" in a:
+                            if nb>gameSet["blue"]:
+                                gameSet["blue"]=nb
+            gamePower.append(reduce(lambda a,b:a*b,gameSet.values()))
+    return reduce(lambda a,b: a+b, gamePower)
 
 
-print(day2_part1())
+print(day2_part2())
