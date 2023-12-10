7fc634f611a2f25c52739ccff83bfa56aaf9a3d4
Langlois Clément <langlois.clement@gmail.com>
2023-12-10 19:12:09 +0100

    day 9 part 1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pkg_resources import resource_stream
 from functools import reduce
 from re import sub, findall, finditer
 from day5 import Day5
-from day7 import Day7, Day7_hand
+from day7 import Day7
 
 print("hello world")
 
@@ -263,9 +263,33 @@ def day8_part2():
         nbstep += 1
     return nbstep
 
+def calcNextValue(listValues: list[int]):
+    flagRec=False
+    for value in listValues:
+        if value!=0:
+            flagRec=True
+            break
+    if not flagRec:
+        return 0
+    ecartValues=[listValues[i+1]-listValues[i] for i in range(len(listValues)-1)]
+    r=listValues[-1]+calcNextValue(ecartValues)
+    return r
+    
+
+
+def day9_part1():
+    print("day 9")
+    histories=[]
+    nextValues=[]
+    with resource_stream('input', 'D9.txt') as textInput:
+        histories = [[int(x) for x in line.decode().strip().split()] for line in textInput.readlines()]
+    for history in histories:
+        nextValues.append(calcNextValue(history))
+    return reduce(lambda a,b: a+b, nextValues)
+
 
 # day5Class = Day5("D5.txt")
 # print(day5Class.part2())
 # day7 = Day7("D7.txt")
 # print(day7.part1())
-print(day8_part2())
+print(day9_part1())
