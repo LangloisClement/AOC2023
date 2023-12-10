91c385144dc0a24fe2cd70a04e5208017cdc7326
Langlois Clément <langlois.clement@gmail.com>
2023-12-10 19:19:35 +0100

    day 9 part 2

--- main.py
+++ main.py
@@ -276,7 +276,6 @@ def calcNextValue(listValues: list[int]):
     return r
     
 
-
 def day9_part1():
     print("day 9")
     histories=[]
@@ -287,9 +286,32 @@ def day9_part1():
         nextValues.append(calcNextValue(history))
     return reduce(lambda a,b: a+b, nextValues)
 
+def calcPreviousValue(listValues: list[int]):
+    flagRec=False
+    for value in listValues:
+        if value!=0:
+            flagRec=True
+            break
+    if not flagRec:
+        return 0
+    ecartValues=[listValues[i+1]-listValues[i] for i in range(len(listValues)-1)]
+    r=listValues[0]-calcPreviousValue(ecartValues)
+    return r
+
+
+def day9_part2():
+    print("day 9")
+    histories=[]
+    nextValues=[]
+    with resource_stream('input', 'D9.txt') as textInput:
+        histories = [[int(x) for x in line.decode().strip().split()] for line in textInput.readlines()]
+    for history in histories:
+        nextValues.append(calcPreviousValue(history))
+    return reduce(lambda a,b: a+b, nextValues)
+
 
 # day5Class = Day5("D5.txt")
 # print(day5Class.part2())
 # day7 = Day7("D7.txt")
 # print(day7.part1())
-print(day9_part1())
+print(day9_part2())
